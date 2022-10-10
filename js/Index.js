2a2a52bd25f9c37e4571e6ee352d8b16f2aa5ee7
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-10-10 23:59:50 +0200

    Added the js for the navbar

--- js/Index.js
+++ js/Index.js
@@ -1,5 +1,7 @@
 let hamburgherMenu = document.querySelector(".hamburger-menu");
+let options = document.querySelector(".options");
 
 hamburgherMenu.addEventListener("click", function () {
     hamburgherMenu.classList.toggle("active");
+    options.classList.toggle("hidden");
 });
